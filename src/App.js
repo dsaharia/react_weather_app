4e29642f403622a4e1bfb46a87f924d90061c22b
Dhiraj Saharia <dsaharia@outlook.com>
2019-03-21 19:02:04 +0530

    added final styling and used stateless fucntions

--- src/App.js
+++ src/App.js
@@ -6,23 +6,71 @@ import Weather from './components/weather'
 const API_KEY = '6f551518ca05837d6e68777f9b8ff9b6';
 
 class App extends React.Component {
+  state = {
+    temperature : undefined,
+    city : undefined,
+    country : undefined,
+    humidity : undefined,
+    description : undefined,
+    error : undefined
+  } // donot directly modify the state use buil in methods
+
   getWeather = async (e) => {
     e.preventDefault(); // event object SEARCH IT!!!
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&mode=json&appid=${API_KEY}`);
     const data = await api_call.json();
-    console.log(data);
+    if (city && country) {
+      // console.log(data);
+      this.setState ({
+        temperature : data.main.temp,
+        city : data.name,
+        country : data.sys.country,
+        humidity : data.main.humidity,
+        description : data.weather[0].description,
+        error : ""
+        });
+    }
+    else {
+      this.setState({
+      temperature : undefined,
+      city : undefined,
+      country : undefined,
+      humidity : undefined,
+      description : undefined,
+      error : "--Please Enter City and Country--"
+
+    });
+    }
   }
   render () {
     return (
       <div>
-        <Titles />
-        <Form getWeather={this.getWeather} />
-        <Weather />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container" >
+                  <Form getWeather={this.getWeather} />
+                      <Weather
+                       temperature={this.state.temperature}
+                       city = {this.state.city}
+                       country = {this.state.country}
+                       humidity = {this.state.humidity}
+                       description = {this.state.description}
+                       error = {this.state.error}
+                      />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>   
     );
   }
 };
-
-export default App;
+        export default App;
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,15 +1,29 @@
 import React from 'react';
+// Using something called a stateless functional components rather than a class based component because
+// Weather does not have any state, no need to import the class, then we cannot use the this keyword
+// and have to pass the props as argument.
 
-class Weather extends React.Component{
-	render() {
-		return (
-			<div>
-				Weather Component
-			</div>
-
-		);
-	}
-
-};
+const Weather = props => 
+	(
+		<div className="weather__info">
+			{
+				props.city && props.country && <p className="weather__key">Location: 
+				<span className="weather__value"> {props.city}, {props.country} </span>
+				</p>
+			}
+			{
+				props.temperature && <p className="weather__key">Temperature: <span className="weather__value"> {props.temperature} </span></p>
+			}
+			{
+				props.humidity &&  <p className="weather__key">Humidity: <span className="weather__value"> {props.humidity} </span></p>
+			}
+			{
+				props.description &&  <p className="weather__key">Conditions: <span className="weather__value"> {props.description} </span></p>
+			}
+			{
+				props.error && <p className="weather__error">Error: {props.error} </p>
+			}
+		</div>
+	);
 
 export default Weather;
